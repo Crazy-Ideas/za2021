5991b44686aae265cf02772e6d0f6194a8a4548f
nayan <nayan@crazyideas.co.in>
2021-08-17 11:25:04 +0530

    Models

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
+import os
+
 from flask import Flask
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "google-cloud.json"
+
+from models import User, Group, Player
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route("/")
+def hello_world():  # put application"s code here
+    return "Hello World!"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
+
+
+@app.shell_context_processor
+def make_shell_context() -> dict:
+    return {
+        "User": User,
+        "Group": Group,
+        "Player": Player,
+    }
---
+++ models.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+
+import pytz
+from firestore_ci import FirestoreDocument
+
+
+class Player(FirestoreDocument):
+
+    def __init__(self):
+        super().__init__()
+        self.name: str = str()
+        self.url: str = str()
+        self.url_expiration: datetime = datetime.now(tz=pytz.UTC)
+        self.qualified: bool = bool()
+        self.league: int = int()
+        self.league_rank: int = int()
+        self.qualification_rank: int = int()
+
+    def __repr__(self):
+        return f"{self.name}"
+
+
+Player.init()
+
+
+class Group(FirestoreDocument):
+
+    def __init__(self):
+        super().__init__()
+        self.group_rank: int = int()
+        self.name: str = str()
+        self.fullname: str = str()
+        self.player_count: int = int()
+        self.player_maps: list = list()
+        self.selection: bool = bool()
+        self.url: str = str()
+        self.url_expiration: datetime = datetime.now(tz=pytz.UTC)
+
+    def __repr__(self):
+        return f"{self.name}:{self.fullname}:P#{self.player_count}"
+
+
+Group.init()
+
+
+class User(FirestoreDocument):
+
+    def __init__(self):
+        super().__init__()
+        self.email: str = str()
+        self.password_hash: str = str()
+        self.token: str = str()
+        self.token_expiration: datetime = datetime.now(tz=pytz.UTC)
+
+
+User.init()
