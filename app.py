a7053e4db358f37964a3f26264f263c93da4de8c
nayan <nayan@crazyideas.co.in>
2022-08-19 11:52:26 +0530

    Fix CSRF token error.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-from base64 import b64encode
 from typing import Optional
 
 from flask import Flask
@@ -8,10 +7,11 @@ from flask_login import LoginManager
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "google-cloud.json"
 
 from models import User
+from secret import SecretConfig
 
 app = Flask(__name__)
 CI_SECURITY: bool = True if os.environ.get("ENVIRONMENT") == "prod" else False
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY") or b64encode(os.urandom(32)).decode()
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY") or SecretConfig.SECRET_KEY
 app.config["SESSION_COOKIE_SECURE"] = CI_SECURITY
 login = LoginManager(app)
 login.login_view = "login"
