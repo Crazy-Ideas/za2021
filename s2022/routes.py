7ea50de67eb830af804098fbec7e74d97da2b038
nayan <nayan@crazyideas.co.in>
2023-02-17 12:50:38 +0530

    rename wc html as s2022

--- s2022/routes.py
+++ s2022/routes.py
@@ -19,7 +19,8 @@ def play_world_cup():
         return redirect(url_for("view_wc_standings"))
     form: PlayWorldCupForm = PlayWorldCupForm(wc_match)
     if not form.validate_on_submit():
-        return render_template("wc_play.html", mt=MarginTag, match_player=wc_match, title="Play World Cup", form=form)
+        return render_template("s2022_play.html", mt=MarginTag, match_player=wc_match, title="Play World Cup",
+                               form=form)
     wc_match.update_result(form.winner.data, form.margin.data)
     return redirect(url_for("s2022.play_world_cup"))
 
@@ -29,7 +30,7 @@ def play_world_cup():
 def view_wc_standings():
     standings: List[Standing] = Standing.objects.filter_by(season=SEASON).get()
     update_rank_and_save(standings, "wc_rank", "wc_score_for_ranking")
-    return render_template("wc_standings.html", standings=standings, title="World Cup 2022 - Standings")
+    return render_template("s2022_standings.html", standings=standings, title="World Cup 2022 - Standings")
 
 
 @bp.route("/s2022/standings/<group_name>")
@@ -39,4 +40,4 @@ def view_wc_players_in_a_group(group_name: str):
     group = Standing.objects.filter_by(group_name=group_name, season=SEASON).first()
     title = f"{group.wc_rank}  {group.group_fullname}   S={group.wc_score}"
     players.sort(key=lambda item: (item.wc_played, item.wc_score), reverse=True)
-    return render_template("wc_players.html", title=title, players=players)
+    return render_template("s2022_players.html", title=title, players=players)
